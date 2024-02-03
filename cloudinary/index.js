7bafbc003d9817223e96800b5486581fb827cfac
Jinesh Van <vjeenesh@gmail.com>
2024-02-03 12:52:09 +0530

    initial commit

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,6 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+// Implementing cloudinary storage for images and invoices
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
